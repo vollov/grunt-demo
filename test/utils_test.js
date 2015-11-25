a9a04b49e62e2a7141232ef7f014cb266ba8ee90
Dustin Zhang <dike.zhang@gmail.com>
2015-11-25 14:33:08 -0500

    corrected file path for utils_test.js

--- test/utils_test.js
+++ test/utils_test.js
@@ -1,18 +1,18 @@
 'use strict';
 
-var Utils = require('../lib/util');
+var Utils = require('../lib/utils');
 
 exports.testConfig = {
 	
 	setUp : function(done) {
 		// setup here
-		this.util = Utils();
+		this.utils = Utils();
 		this.docs = [{"_id":"54a4d108228799901cb889e5","name":"Kayak", "pwd":"aaa"},
 		    		 {"_id":"54a4d108228799901cb889e6","name":"Lifejacket", "pwd":"aaa"}]
 		done();
 	},
 	'test removeFields' : function(test) {
-		var result = this.util.removeFields(this.docs, 'pwd');
+		var result = this.utils.removeFields(this.docs, 'pwd');
 		var expected  = [{"name":"Kayak","_id":"54a4d108228799901cb889e5"},
 		                 {"name":"Lifejacket","_id":"54a4d108228799901cb889e6"}]
 		test.equal(result, expected, 'pwd field should be removed.');
